b402c913bc8ce23e0050f70b98d4f97a716bd985
abdellatif Elsheikh <elshikh532@gmail.com>
2021-09-29 00:57:07 +0200

    add event listners to manipulate the elements

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Define Global Variables
-const countriesList = document.querySelector(".draggable-list");
-const checkOrder = document.querySelector(".check-order");
 let dragStartIndex;
 let shuffledCountries;
+const listItems = [];
+const countriesList = document.querySelector(".draggable-list");
+const checkOrder = document.querySelector(".check-order");
 
 // Array of larges countries in the correct order
 const largestCountries = [
@@ -18,19 +19,33 @@ const largestCountries = [
     "Algeria",
 ];
 
-// Array to store items
-const listItems = [];
+/***
+ *  ---------- Start helper functions ----------
+***/
 
-// Start helper functions
 function setAttributes(el, attrs) {
     for (let key in attrs) {
         el.setAttribute(key, attrs[key]);
     }
 }
 
-// End helper functions
+// Swap elements in the dom
+function swapItems(from, to){
+    const itemOne = listItems[from].querySelector(".draggble-item");
+    const itemTwo = listItems[to].querySelector(".draggble-item");
 
-// Start main functions
+    listItems[from].appendChild(itemTwo);
+    listItems[to].appendChild(itemOne);
+    console.log(itemOne, itemTwo);
+}
+
+/***
+ * ---------- End helper functions ----------
+***/
+
+/***
+ * ---------- Start main functions ----------
+***/
 // Insert list items into ul
 function createCountriesList() {
     // Create Unordered Countres Array
@@ -42,10 +57,10 @@ function createCountriesList() {
         const cName = document.createElement("p");
         const icon = document.createElement("i");
         // Fill new elements
-        setAttributes(listItem,{'data-index': index, 'class':"list-item"});
+        setAttributes(listItem, { 'data-index': index, 'class': "list-item", 'draggable': "true" });
         cNumber.setAttribute("class", "country-number");
         cNumber.innerText = index + 1
-        setAttributes(draggableEle, {'class':"draggble-item", 'draggable':"true"});
+        setAttributes(draggableEle, { 'class': "draggble-item", 'draggable': "true" });
         cName.setAttribute("class", "country-name");
         cName.innerText = country;
         icon.setAttribute("class", "fas fa-grip-lines")
@@ -58,18 +73,91 @@ function createCountriesList() {
         // Push elements to new array
         listItems.push(listItem)
     });
+
+    addEvenetLlistners()
 }
 
 // Sjuffle the list of countries
-function shuffleTheList(){
-    shuffledCountries= largestCountries.map(a => ({
-        value: a, sort: Math.random()
+function shuffleTheList() {
+    shuffledCountries = largestCountries.map(country => ({
+        value: country, sort: Math.random()
     }))
-    .sort((a,b)=> a.sort - b.sort)
-    .map(a => a.value);
+        .sort((a, b) => a.sort - b.sort)
+        .map(a => a.value);
 
-    createCountriesList()
+    createCountriesList();
 }
 shuffleTheList();
 
-// End main functions
+/**
+ * Start Draggable functions
+**/
+
+function dragStart(e) {
+    console.log("dragstart", +this.closest("li").getAttribute("data-index"));
+    dragStartIndex = Number(this.closest("li").getAttribute("data-index"))
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+function dragEnter() {
+    this.classList.add("over")
+}
+function dragLeave() {
+    this.classList.remove("over");
+}
+function dragDrop() {
+    const dragEndIndex = Number(this.getAttribute("data-index"));
+    this.classList.remove("over");
+    swapItems(dragStartIndex,dragEndIndex)
+    console.log(dragEndIndex);
+}
+
+/**
+ * end draggable functions
+**/
+
+// Check of the correct order 
+function checCorrectkOrder(e){
+    listItems.forEach((item, index) => {
+        const rightOrder = largestCountries[index];
+        const currentOrder =  item.querySelector(".draggble-item").textContent;
+
+        if(rightOrder === currentOrder){
+            item.classList.add("right")
+            item.classList.remove("wrong")
+        }else{
+            item.classList.add("wrong")
+            item.classList.remove("right")
+        }
+    })
+}
+
+
+/***
+ * ---------- End main functions ----------
+***/
+
+/***
+ * ---------- add event listners ----------
+***/
+
+// Add events for drag to each item
+function addEvenetLlistners() {
+    const listItems = document.querySelectorAll(".list-item");
+    const draggbleItems = document.querySelectorAll(".draggble-item");
+
+    draggbleItems.forEach(item => {
+        item.addEventListener("dragstart", dragStart)
+        item.addEventListener("dragenter", dragEnter);
+        item.addEventListener("dragleave", dragLeave);
+    });
+    
+    listItems.forEach(listItem => {
+        listItem.addEventListener("dragover", dragOver);
+        listItem.addEventListener("drop", dragDrop);
+    });
+}
+
+checkOrder.onclick = checCorrectkOrder
